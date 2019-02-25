e0a5bb8b27d40c709f36c70fa0837726397cf2c4
Antony Montalvo <antonis162010@gmail.com>
2019-02-24 23:01:09 -0500

    Crud MEAN with GraphQL end

--- src/graphql/resolvers/employeeResolver.js
+++ src/graphql/resolvers/employeeResolver.js
@@ -6,13 +6,32 @@ module.exports = {
       return await Employee.find({});
     },
     employee: async (obj, args) => {
-      return await Employee.findOne(args);
+      return await Employee.findOne(args); //args representa un { _id: args._id }
     }
   },
   Mutation: {
     addEmployee: async (obj, args) => {
       const employee = new Employee(args);
       return await employee.save();
+    },
+    updateEmployee: async (obj, args) => {
+      const employee = {
+        name: args.name,
+        position: args.position,
+        office: args.office,
+        salary: args.salary
+      };
+
+      return await Employee.findByIdAndUpdate(
+        //El args._id funciona igual que { _id: args._id } para mongoDB
+        { _id: args._id },
+        { $set: employee },
+        { new: true }
+      );
+    },
+    deleteEmployee: async (obj, args) => {
+      const res = await Employee.findByIdAndRemove(args._id);
+      return res == null ? "This id not exists" : "Delete succesfully";
     }
   }
 };
--- src/models/database.js
+++ src/models/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const URI = "mongodb://localhost/DatabaseMEAN";
 
 mongoose
-  .connect( URI, { useNewUrlParser: true } )
-  .then(db => console.log("DB is connected"))
+  .connect(URI, { useNewUrlParser: true })
+  .then(db => console.log(db.connections[0].db.s.databaseName, " is connected"))
   .catch(err => console.error(err));
 
 module.exports = mongoose;
