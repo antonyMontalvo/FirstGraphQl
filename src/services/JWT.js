019e154ddeaf027056e154500e3d00a69005759b
Antony <amontalvo.hdc@gmail.con>
2019-02-25 14:15:11 -0500

    Cambio

---
+++ src/services/JWT.js
@@ -0,0 +1,16 @@
+const jwt =  require('jsonwebtoken'),
+    token = "123456789";
+
+const JWT = {};
+
+JWT.createUserToken = (user) => {
+    const payload = user
+    return jwt.sign(payload, token);
+}
+
+JWT.getPayload = ( bearerToken ) => {
+    let token = bearerToken.split(" ")[1];
+    return jwt.decode( token )
+}
+
+module.exports = JWT;
